86d4c0e9b1d2088fca596565cce8f48389d257ba
praveen-golagani <praveen.gyj@gmail.com>
2025-01-23 08:35:03 +0530

    star pattern

---
+++ practice1/basics1.js
@@ -0,0 +1,14 @@
+console.log("malli modalinde")
+var a = 4 // es4 we have var
+let b = 6 // from es6
+const max_marks = 100 // from es6
+console.log(typeof(a)) //data type
+console.log(a)
+let flag = true
+console.log(typeof(flag))
+// null and undefined
+// var vs let, we cannot redeclare variable with let keyword, but its possible with var
+// redeclare is diff from reassigning
+//reassign and redeclare is possible in var
+// only reassign is possible in let
+//const cannot be reassigned and redeclare
---
+++ practice1/basics2.js
@@ -0,0 +1,16 @@
+const flag = true
+ if(flag){
+    console.log("fun 1 true")
+ }else{
+    console.log("fun2 rock")
+ }
+
+ for (let i=0;i<5;i++){
+    console.log(i)
+ }
+
+ i=0
+ while(i<3){
+console.log(i);
+i++;
+ }
